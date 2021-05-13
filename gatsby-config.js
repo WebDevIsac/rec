1af84f2b99fa705fb22e2b2381a3acae1971773b
Isac Larsson <isaclarsson98@gmail.com>
2021-05-13 10:41:07 +0200

    Install and use root import package

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,18 @@
+const path = require('path');
+
 module.exports = {
-  siteMetadata: {
-    title: "My Recipes",
-  },
-  plugins: ["gatsby-plugin-emotion"],
+    siteMetadata: {
+        title: 'My Recipes',
+    },
+    plugins: [
+        'gatsby-plugin-emotion',
+        {
+            resolve: 'gatsby-plugin-root-import',
+            options: {
+                src: path.join(__dirname, 'src'),
+                pages: path.join(__dirname, 'src/pages'),
+                components: path.join(__dirname, 'src/components'),
+            },
+        },
+    ],
 };
