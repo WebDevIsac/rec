a3b35cae6ff28e5609d3b9c26559cb5386151a86
Isac Larsson <isaclarsson98@gmail.com>
2021-05-13 11:48:41 +0200

    Create header and footer with some styling

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Footer = props => {
+    return (
+        <div>
+            <ul>
+                <li>Instagram</li>
+                <li>Facebook</li>
+                <li>LinkedIn</li>
+            </ul>
+        </div>
+    );
+};
+
+Footer.propTypes = {};
+
+export { Footer };
---
+++ src/components/Header.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'gatsby';
+import { styled } from 'styletron-react';
+
+const StyledHeader = styled('header', {
+    height: '100px',
+});
+
+const Navbar = styled('nav', {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+});
+
+const Item = styled('div', {
+    display: 'inline-block',
+});
+
+const StyledLink = styled(Link, {
+    color: 'black',
+    textDecoration: 'none',
+    fontSize: '24px',
+    padding: '0 12px',
+});
+
+const Header = props => {
+    return (
+        <StyledHeader>
+            <Navbar>
+                <Item>
+                    <StyledLink to="/">Hem</StyledLink>
+                </Item>
+                <Item>
+                    <StyledLink to="/contact">Kontakt</StyledLink>
+                </Item>
+                <Item>
+                    <StyledLink to="/about">Om oss</StyledLink>
+                </Item>
+            </Navbar>
+        </StyledHeader>
+    );
+};
+
+Header.propTypes = {};
+
+export { Header };
